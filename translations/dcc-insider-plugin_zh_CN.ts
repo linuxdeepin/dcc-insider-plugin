daea7475846685ace26f9c4b839e4c3a88c0d201
rewine <luhongxu@deepin.org>
2023-09-21 10:03:05 +0800

    feat: support ddm/treeland preview
    Log: support ddm/treeland preview

--- translations/dcc-insider-plugin_zh_CN.ts
+++ translations/dcc-insider-plugin_zh_CN.ts
@@ -4,24 +4,39 @@
 <context>
     <name>InsiderModule</name>
     <message>
-        <location filename="../insidermodule.cpp" line="19"/>
+        <location filename="../insidermodule.cpp" line="29"/>
         <source>Technology Preview</source>
         <translation>技术预览</translation>
     </message>
     <message>
-        <location filename="../insidermodule.cpp" line="25"/>
+        <location filename="../insidermodule.cpp" line="36"/>
         <source>Currently stable launcher (dde-launcher)</source>
         <translation>当前稳定版启动器（dde-launcher）</translation>
     </message>
     <message>
-        <location filename="../insidermodule.cpp" line="29"/>
+        <location filename="../insidermodule.cpp" line="40"/>
         <source>Technology preview launcher (dde-launchpad)</source>
         <translation>技术预览版启动器（dde-launchpad）</translation>
     </message>
     <message>
-        <location filename="../insidermodule.cpp" line="33"/>
+        <location filename="../insidermodule.cpp" line="44"/>
         <source>New Launcher</source>
         <translation>新版启动器</translation>
+    </message>
+    <message>
+        <location filename="../insidermodule.cpp" line="71"/>
+        <source>Currently stable Display Manager (lightdm)</source>
+        <translation>当前稳定版显示管理器（lightdm）</translation>
+    </message>
+    <message>
+        <location filename="../insidermodule.cpp" line="75"/>
+        <source>Technology preview Display Manager/Window Manager (ddm/treeland)</source>
+        <translation>技术预览版显示管理器/窗口管理器(ddm/treeland)</translation>
+    </message>
+    <message>
+        <location filename="../insidermodule.cpp" line="79"/>
+        <source>New Display Manager</source>
+        <translation>新版显示管理器</translation>
     </message>
 </context>
 </TS>
