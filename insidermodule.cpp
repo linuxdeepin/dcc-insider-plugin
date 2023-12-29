de2b0e028ac284119c78df5c429042b72e7e27db
quezhiyong <quezhiyong@deepin.org>
2023-12-29 16:23:19 +0800

    feat: support dim preview
    support dim preview

--- insidermodule.cpp
+++ insidermodule.cpp
@@ -18,6 +18,7 @@
 #include <QProcess>
 #include <QVBoxLayout>
 #include <QProcess>
+#include <QDir>
 
 // PackageKit-Qt
 #include <Daemon>
@@ -62,6 +63,36 @@ InsiderModule::InsiderModule(QObject *parent)
 
     appendChild(m_dmList);
 
+    // Input Method
+    m_availableIM = new QStandardItemModel(this);
+
+    DStandardItem *fcitx5= new DStandardItem(tr("Currently stable Input Method"));
+    fcitx5->setData("fcitx5", Dtk::UserRole);
+    m_availableIM->appendRow(fcitx5); // role: package name
+
+    DStandardItem *dim = new DStandardItem(tr("Technology preview Input Method (deepin-im)"));
+    dim->setData("deepin-im", Dtk::UserRole);
+    m_availableIM->appendRow(dim);
+
+    appendChild(new ItemModule("imTitle", tr("New Input Method")));
+    m_imList = new ItemModule(
+        "selectInputMethod", "",
+        [this](ModuleObject *) -> QWidget * {
+            auto *imListView = new DCCListView();
+            imListView->setModel(m_availableIM);
+
+            connect(imListView, &DListView::clicked, this, [this](const QModelIndex &index) {
+                QStandardItem *checkedItem = m_availableIM->itemFromIndex(index);
+                QString packageName = checkedItem->data(Dtk::UserRole).toString();;
+                installInputMethod(packageName);
+            });
+
+            return imListView;
+        },
+        false
+    );
+    appendChild(m_imList);
+
 //    qDebug() << PackageKit::Daemon::isRunning();
 //    connect(PackageKit::Daemon::global(), &PackageKit::Daemon::isRunningChanged, this,
 //            [this](){
@@ -84,6 +115,7 @@ InsiderModule::~InsiderModule()
 void InsiderModule::active()
 {
     checkEnabledDisplayManager();
+    checkEnabledInputMethod();
 }
 
 void InsiderModule::deactive()
@@ -110,6 +142,24 @@ void InsiderModule::installDisplayManager(const QString packageName)
     });
 }
 
+static bool imConfigIsDim() {
+    auto home = QDir::home();
+    QFile file(home.filePath(".xinputrc"));
+   
+    if (file.exists() && file.open(QFile::ReadOnly)) {
+        QTextStream in (&file);
+        QString line;
+        do {
+            line = in.readLine();
+            if (!line.contains("run_im dim")) {
+                return true;
+            }
+        } while (!line.isNull());
+    }
+
+    return false;
+}
+
 void InsiderModule::checkEnabledDisplayManager()
 {
     QProcess process;
@@ -125,6 +175,21 @@ void InsiderModule::checkEnabledDisplayManager()
         QStandardItem * item = m_availableDm->item(i);
         bool isChecked = isLightdm == (item->data(Dtk::UserRole).toString()=="lightdm");
         item->setCheckState(isChecked ? Qt::Checked : Qt::Unchecked);
+    }
+
+    if (isLightdm) {
+        if (imConfigIsDim()) {
+            switchInputMethod(false);
+            checkEnabledInputMethod();
+        }
+    }
+
+    int availableIMCount = m_availableIM->rowCount();
+    for (int i = 0; i < availableIMCount; i++) {
+        QStandardItem *item = m_availableIM->item(i);
+        if (item->data(Dtk::UserRole).toString() == "deepin-im") {
+            item->setEnabled(!isLightdm);
+        }
     }
 }
 
@@ -143,3 +208,45 @@ void InsiderModule::switchDisplayManager(bool isNew)
     qDebug() << "switchDisplayManager: " << process.readAll();
 }
 
+void InsiderModule::installInputMethod(const QString &packageName) {
+    bool isNew = packageName == "deepin-im";
+
+    qWarning() << "package:" << packageName;
+    PKUtils::resolve(packageName).then([this, isNew] (const PKUtils::PkPackages packages) {
+        if (packages.isEmpty()) return;
+
+        PKUtils::installPackage(packages.constFirst()).then([this, isNew]() {
+            switchInputMethod(isNew);
+            checkEnabledInputMethod();
+        }, [this](const std::exception & e) {
+            PKUtils::PkError::printException(e);
+            checkEnabledInputMethod();
+        });
+    });
+}
+
+void InsiderModule::checkEnabledInputMethod() {
+    bool isDim = imConfigIsDim();
+
+    int availableIMCount = m_availableIM->rowCount();
+    for (int i = 0; i < availableIMCount; i++) {
+        QStandardItem *item = m_availableIM->item(i);
+        bool isChecked = isDim == (item->data(Dtk::UserRole).toString() == "deepin-im");
+        item->setCheckState(isChecked ? Qt::Checked : Qt::Unchecked);
+    }
+}
+
+void InsiderModule::switchInputMethod(bool isNew) {
+    if (isNew) {
+        QProcess process;
+        process.setProgram("im-config");
+        process.setArguments({"-n", "dim"});
+        process.start();
+        process.waitForFinished();
+    } else {
+        if (imConfigIsDim()) {
+            QDir home = QDir::home();
+            home.remove(".xinputrc");
+        }
+    }
+}
