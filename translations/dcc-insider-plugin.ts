fd2f3e6178e51d44456a71c4d3a9d1ddee5d7fd8
quezhiyong <quezhiyong@deepin.org>
2024-01-05 10:44:13 +0800

    chore: add Chinese translations
    add Chinese translations

--- translations/dcc-insider-plugin.ts
+++ translations/dcc-insider-plugin.ts
@@ -4,38 +4,38 @@
 <context>
     <name>InsiderModule</name>
     <message>
-        <location filename="../insidermodule.cpp" line="29"/>
+        <location filename="../insidermodule.cpp" line="30"/>
         <source>Technology Preview</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../insidermodule.cpp" line="36"/>
-        <source>Currently stable launcher (dde-launcher)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../insidermodule.cpp" line="40"/>
-        <source>Technology preview launcher (dde-launchpad)</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../insidermodule.cpp" line="44"/>
-        <source>New Launcher</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../insidermodule.cpp" line="71"/>
+        <location filename="../insidermodule.cpp" line="37"/>
         <source>Currently stable Display Manager (lightdm)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../insidermodule.cpp" line="75"/>
+        <location filename="../insidermodule.cpp" line="41"/>
         <source>Technology preview Display Manager/Window Manager (ddm/treeland)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../insidermodule.cpp" line="79"/>
+        <location filename="../insidermodule.cpp" line="45"/>
         <source>New Display Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../insidermodule.cpp" line="69"/>
+        <source>Currently stable Input Method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../insidermodule.cpp" line="73"/>
+        <source>Technology preview Input Method (deepin-im)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../insidermodule.cpp" line="77"/>
+        <source>New Input Method</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- translations/dcc-insider-plugin_zh_CN.ts
+++ translations/dcc-insider-plugin_zh_CN.ts
@@ -4,39 +4,51 @@
 <context>
     <name>InsiderModule</name>
     <message>
-        <location filename="../insidermodule.cpp" line="29"/>
+        <location filename="../insidermodule.cpp" line="30"/>
         <source>Technology Preview</source>
         <translation>技术预览</translation>
     </message>
     <message>
-        <location filename="../insidermodule.cpp" line="36"/>
         <source>Currently stable launcher (dde-launcher)</source>
-        <translation>当前稳定版启动器（dde-launcher）</translation>
+        <translation type="vanished">当前稳定版启动器（dde-launcher）</translation>
     </message>
     <message>
-        <location filename="../insidermodule.cpp" line="40"/>
         <source>Technology preview launcher (dde-launchpad)</source>
-        <translation>技术预览版启动器（dde-launchpad）</translation>
+        <translation type="vanished">技术预览版启动器（dde-launchpad）</translation>
     </message>
     <message>
-        <location filename="../insidermodule.cpp" line="44"/>
         <source>New Launcher</source>
-        <translation>新版启动器</translation>
+        <translation type="vanished">新版启动器</translation>
     </message>
     <message>
-        <location filename="../insidermodule.cpp" line="71"/>
+        <location filename="../insidermodule.cpp" line="37"/>
         <source>Currently stable Display Manager (lightdm)</source>
         <translation>当前稳定版显示管理器（lightdm）</translation>
     </message>
     <message>
-        <location filename="../insidermodule.cpp" line="75"/>
+        <location filename="../insidermodule.cpp" line="41"/>
         <source>Technology preview Display Manager/Window Manager (ddm/treeland)</source>
         <translation>技术预览版显示管理器/窗口管理器(ddm/treeland)</translation>
     </message>
     <message>
-        <location filename="../insidermodule.cpp" line="79"/>
+        <location filename="../insidermodule.cpp" line="45"/>
         <source>New Display Manager</source>
         <translation>新版显示管理器</translation>
+    </message>
+    <message>
+        <location filename="../insidermodule.cpp" line="69"/>
+        <source>Currently stable Input Method</source>
+        <translation>当前稳定版输入法</translation>
+    </message>
+    <message>
+        <location filename="../insidermodule.cpp" line="73"/>
+        <source>Technology preview Input Method (deepin-im)</source>
+        <translation>技术预览版输入法（deepin-im）</translation>
+    </message>
+    <message>
+        <location filename="../insidermodule.cpp" line="77"/>
+        <source>New Input Method</source>
+        <translation>新版输入法</translation>
     </message>
 </context>
 </TS>
