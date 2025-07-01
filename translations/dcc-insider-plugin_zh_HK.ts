69a90681336ec07034668db04519a5f970a4a972
Wang Zichong <wangzichong@deepin.org>
2025-07-01 17:46:47 +0800

    chore(i18n): add zh_HK and zh_TW translations
    添加繁体中文翻译文案资源文件
    
    Log:

---
+++ translations/dcc-insider-plugin_zh_HK.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS language="zh_HK" version="2.1">
+    <context>
+        <name>InsiderModule</name>
+        <message>
+            <location filename="../dcc-insider-plugin/insidermodule.cpp" line="30"/>
+            <source>Technology Preview</source>
+            <translation>技術預覽</translation>
+        </message>
+        <message>
+            <location filename="../dcc-insider-plugin/insidermodule.cpp" line="37"/>
+            <source>Currently stable Display Manager (lightdm)</source>
+            <translation>當前穩定版顯示管理器（lightdm）</translation>
+        </message>
+        <message>
+            <location filename="../dcc-insider-plugin/insidermodule.cpp" line="41"/>
+            <source>Technology preview Display Manager/Window Manager (ddm/treeland)</source>
+            <translation>技術預覽版顯示管理器/窗口管理器(ddm/treeland)</translation>
+        </message>
+        <message>
+            <location filename="../dcc-insider-plugin/insidermodule.cpp" line="45"/>
+            <source>New Display Manager</source>
+            <translation>新版顯示管理器</translation>
+        </message>
+        <message>
+            <location filename="../dcc-insider-plugin/insidermodule.cpp" line="72"/>
+            <source>Currently stable Input Method</source>
+            <translation>當前穩定版輸入法</translation>
+        </message>
+        <message>
+            <location filename="../dcc-insider-plugin/insidermodule.cpp" line="76"/>
+            <source>Technology preview Input Method (deepin-im)</source>
+            <translation>技術預覽版輸入法（deepin-im）</translation>
+        </message>
+        <message>
+            <location filename="../dcc-insider-plugin/insidermodule.cpp" line="80"/>
+            <source>New Input Method</source>
+            <translation>新版輸入法</translation>
+        </message>
+    </context>
+</TS>
